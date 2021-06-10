ede6370229588ed1ee36ad4e50074f8485513a45
Ryan Lee <ryanlee059@gmail.com>
2021-06-09 17:28:56 -0700

    Fixed async/await syntax

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
@@ -38,14 +38,14 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   const tagData = await Tag.create(req.body);
 
   return res.json(tagData);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   const tagData = await Tag.update(
     {
@@ -61,7 +61,7 @@ router.put('/:id', (req, res) => {
   return res.json(tagData);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
